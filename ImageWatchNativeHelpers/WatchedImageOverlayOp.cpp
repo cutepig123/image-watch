9f25edd2f36ec1be178c7b65c7f4c4094af4d4e4
hjs <hjs>
2026-05-27 23:43:33 +0800

    feat(overlay): improve WatchedImageOverlayOp implementation
    - Update project file to include WatchedImageOverlayOp
    - Refactor to use WatchedImageOperator base class
    - Add placeholder for overlay data parsing
    - The operator now properly handles image + overlay expression arguments

--- ImageWatchNativeHelpers/WatchedImageOverlayOp.cpp
+++ ImageWatchNativeHelpers/WatchedImageOverlayOp.cpp
@@ -1,35 +1,123 @@
 #include "WatchedImageOverlayOp.h"
+#include "WatchedImageHelpers.h"
+#include "WatchedImageImage.h"
+
+#include <Microsoft.VisualStudio.Debugger.Evaluation.DkmInspectionContext.h>
+#include <Microsoft.VisualStudio.Debugger.CallStack.DkmStackWalkFrame.h>
 
 using namespace System;
+using namespace Microsoft::Research::NativeImage;
 
 namespace Microsoft 
 {
     namespace ImageWatch
     {
         WatchedImageOverlayOp::WatchedImageOverlayOp()
-            : WatchedImageBinaryOp()
+            : WatchedImageOperator(), sourceImage_(nullptr), overlayDataExpr_(nullptr)
         {
+        }
+        
+        WatchedImageOverlayOp::~WatchedImageOverlayOp()
+        {
+            this->!WatchedImageOverlayOp();
+        }
+        
+        WatchedImageOverlayOp::!WatchedImageOverlayOp()
+        {
+        }
+        
+        array<Type^>^ WatchedImageOverlayOp::DoGetArgumentTypes()
+        {
+            // Two arguments: image and overlay data expression
+            auto res = gcnew array<Type^>(2);
+            res[0] = WatchedImage::typeid;  // Image
+            res[1] = String::typeid;         // Overlay data expression
+            return res;
+        }
+        
+        vt::CTransformGraphNode* WatchedImageOverlayOp::GetTransformGraphHead()
+        {
+            // Overlay doesn't use Transform Graph
+            // We return the source image's transform graph
+            if (!CheckArguments() || sourceImage_ == nullptr)
+                return nullptr;
+            
+            auto op = dynamic_cast<WatchedImageOperator^>(sourceImage_);
+            if (op != nullptr)
+            {
+                return op->GetTransformGraphHead();
+            }
+            
+            return nullptr;
         }
         
         void WatchedImageOverlayOp::DoReloadInfo(WatchedImageInfo^% info)
         {
-            if (Operand1 != nullptr)
-            {
-                Operand1->ReloadInfo();
-                info = Operand1->GetInfo();
-            }
+            info = nullptr;
+            
+            if (!CheckArguments())
+                return;
+            
+            // First argument is the image
+            sourceImage_ = dynamic_cast<WatchedImage^>(Args[0]);
+            if (sourceImage_ == nullptr)
+                return;
+            
+            // Second argument is overlay data expression
+            overlayDataExpr_ = dynamic_cast<String^>(Args[1]);
+            
+            // Reload source image info
+            sourceImage_->ReloadInfo();
+            
+            if (!sourceImage_->HasValidInfo())
+                return;
+            
+            // Use source image's info
+            info = sourceImage_->GetInfo();
         }
         
         void WatchedImageOverlayOp::DoReloadPixels(bool% hasPixelsLoaded)
         {
-            if (Operand1 != nullptr)
-            {
-                Operand1->ReloadPixels();
-                hasPixelsLoaded = Operand1->HasPixelsLoaded();
-            }
+            hasPixelsLoaded = false;
             
-            // TODO: Parse overlay data from Operand2 and add primitives
-            // This would require debugger expression evaluation
+            if (sourceImage_ == nullptr || !sourceImage_->HasValidInfo())
+                return;
+            
+            // Reload source image pixels
+            sourceImage_->ReloadPixels();
+            hasPixelsLoaded = sourceImage_->HasPixelsLoaded();
+            
+            if (!hasPixelsLoaded)
+                return;
+            
+            // Parse and apply overlay
+            ParseOverlayData();
+        }
+        
+        void WatchedImageOverlayOp::ParseOverlayData()
+        {
+            // This is a simplified implementation
+            // In a full implementation, we would:
+            // 1. Evaluate overlayDataExpr_ in the debugger context
+            // 2. Parse the structure (points, lines, arcs)
+            // 3. Extract coordinates and colors
+            // 4. Call NativeImageView::AddOverlayPoint/Line/Arc
+            
+            // For now, this is a placeholder that demonstrates the concept
+            // Full implementation requires deep integration with VS Debugger API
+            
+#ifdef _DEBUG
+            System::Diagnostics::Debug::WriteLine(
+                "WatchedImageOverlayOp::ParseOverlayData: Overlay expression: {0}", 
+                overlayDataExpr_);
+#endif
+        }
+        
+        void WatchedImageOverlayOp::ApplyOverlayToView()
+        {
+            // This would be called after rendering to add overlay
+            // The actual implementation needs access to NativeImageView
+            // which is managed at a higher level in the UI layer
         }
     }
 }
--- ImageWatchNativeHelpers/WatchedImageOverlayOp.h
+++ ImageWatchNativeHelpers/WatchedImageOverlayOp.h
@@ -1,19 +1,36 @@
 #pragma once
 
-#include "WatchedImageBinaryOp.h"
+#include "WatchedImage.h"
+#include "WatchedImageOperator.h"
 
 namespace Microsoft 
 {
     namespace ImageWatch
     {
-        public ref class WatchedImageOverlayOp : public WatchedImageBinaryOp
+        // @overlay(image, overlayData) operator
+        // This operator is special: it doesn't use Transform Graph
+        // Instead, it passes overlay data to NativeImageView for rendering
+        public ref class WatchedImageOverlayOp : public WatchedImageOperator
         {
         public:
             WatchedImageOverlayOp();
+            ~WatchedImageOverlayOp();
+            !WatchedImageOverlayOp();
+            
+        internal:
+            virtual vt::CTransformGraphNode* GetTransformGraphHead() override;
             
         protected:
+            virtual array<Type^>^ DoGetArgumentTypes() override;
             virtual void DoReloadInfo(WatchedImageInfo^% info) override;
             virtual void DoReloadPixels(bool% hasPixelsLoaded) override;
+            
+        private:
+            WatchedImage^ sourceImage_;
+            System::String^ overlayDataExpr_;
+            
+            void ParseOverlayData();
+            void ApplyOverlayToView();
         };
     }
 }
